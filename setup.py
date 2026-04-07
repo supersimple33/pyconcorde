1090b0e38f90d1b781166064947008d66511a123
Addison Hanrattie <40609224+supersimple33@users.noreply.github.com>
2026-04-06 23:21:39 -0400

    TEMP2

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ QSOPT_LOCATION = {
     },
 }
 
-CONCORDE_SRC = "https://www.dropbox.com/scl/fi/rfmevjdamivlm5seqlogd/co031219.tgz?rlkey=8xn6r2kap7nbv55no6f9tm7re&e=1&st=se421n3h&dl=1"  # noqa
+CONCORDE_SRC = "https://www.dropbox.com/scl/fi/9ytevb298og5u7tmjgyc9/co031219.tgz?rlkey=co9z9n6kxq5ir140zz5669jzt&st=2u8nhhaj&dl=1"  # noqa
 
 
 def _safe_makedirs(*paths):
